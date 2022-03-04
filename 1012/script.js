f6c169dcfd82293e6c7aa373f4c44e3d1cdb4342
LUCIO Notebook <luciononato@hotmail.com>
2022-03-04 20:58:00 -0300

    resolução 1012

--- 1012/script.js
+++ 1012/script.js
@@ -9,19 +9,19 @@ const valores = input.split("\n")
 const [A, B, C] = valores[0].split(" ")
 
 // Área do triângulo retângulo
-const areaTriangulo = (A*C)/2
+const areaTriangulo = (parseFloat(A)*parseFloat(C))/2
 
 // A área do círculo
 const pi = 3.14159
-const areaCirculo = pi*Math.pow(C, 2)
+const areaCirculo = pi*Math.pow(parseFloat(C), 2)
 
 // a área do trapézio
 const areaTrapezio = (parseFloat(A)+ parseFloat(B))*parseFloat(C)/2
 
 // a área do quadrado 
-const areaQuadrado = Math.pow(B, 2)
+const areaQuadrado = Math.pow(parseFloat(B), 2)
 
 // a área do retângulo
-const areaRetangulo = A*B
+const areaRetangulo = parseFloat(A)*parseFloat(B)
 
 console.log(`TRIANGULO: ${areaTriangulo.toFixed(3)}\n`, `CIRCULO: ${areaCirculo.toFixed(3)}\n`, `TRAPEZIO: ${areaTrapezio.toFixed(3)}\n`, `QUADRADO: ${areaQuadrado.toFixed(3)}\n`, `RETANGULO: ${areaRetangulo.toFixed(3)}`)
